d6df32a6fbcf1a8fe7e9677f5ea7afa6c89588fd
m4nogyad <bajracharyamanogya@gmail.com>
2026-08-30 22:23:11 +0545

    made home page and added footer and other pages

--- backend/TMS/TMS/settings.py
+++ backend/TMS/TMS/settings.py
@@ -130,7 +130,8 @@ MAILERS = {
     },
 }
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # or 5173 for Vite
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 MEDIA_URL = '/media/'
